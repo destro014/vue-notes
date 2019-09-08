cd23c59d55ebbc5b9b15724fbd16ba8b3a3c1500
Destro <destro014@users.noreply.github.com>
2019-09-08 17:45:15 +0545

    remove api key

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -4,13 +4,10 @@ import firestore from 'firebase/firestore'
 
 // Initialize Firebase
 var firebaseConfig = {
-  apiKey: "AIzaSyBUlPj9SRXq6B2LeMddafV0bkQRb02zUIU",
-  authDomain: "destro-notes.firebaseapp.com",
-  databaseURL: "https://destro-notes.firebaseio.com",
-  projectId: "destro-notes",
-  // storageBucket: "",
-  // messagingSenderId: "359133466712",
-  appId: "1:359133466712:web:dbb3fbc4078ffc83c9ba2d"
+  apiKey: "your_api_key",
+  authDomain: "name.firebaseapp.com",
+  databaseURL: "https://name.firebaseio.com",
+  projectId: "vue-notes"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
